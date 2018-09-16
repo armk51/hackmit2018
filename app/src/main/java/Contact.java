2c26b36bf81e5f6a918f23bf637d993da134bd88
armk51 <armk5100@gmail.com>
2018-09-15 22:05:27 -0400

    Started init for contact.

---
+++ app/src/main/java/Contact.java
@@ -0,0 +1,16 @@
+public class Contact {
+    private String APIKey = "da328055e2e940d8b28055e2e9e0d851";
+    private String name;
+    private String phn;
+    private String address;
+    private double x;
+    private double y;
+
+    public Contact (String name, String phn, String address) {
+        this.name = name;
+        this.phn = phn;
+        this.address = address;
+
+
+    }
+}
